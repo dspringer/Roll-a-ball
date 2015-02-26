d38cd3aefa1ab351898aeec9982012d8b5d3d2ea
Darian Springer <darianc.springer@gmail.com>
2015-02-26 17:59:57 -0500

    Reads multiple lines from text file to create more objects

--- Assets/Scripts/Instantiation.cs
+++ Assets/Scripts/Instantiation.cs
@@ -12,7 +12,7 @@ public class Instantiation : MonoBehaviour {
 			for (int x = 0; x < 5; x++) {
 				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 				cube.AddComponent<Rigidbody>();
-				//cube.transform.localScale = new Vector3(.25f, .25f, .25f);
+				cube.transform.localScale = new Vector3(.25f, .25f, .25f);
 				cube.transform.position = new Vector3(x, y, 0);
 			}
 		}
@@ -23,16 +23,19 @@ public class Instantiation : MonoBehaviour {
 
 
 		// Currently a performance bottleneck? Later should read-in in bulk
-		var tokens = sr.ReadLine ().Trim().Split (',');
-		for (int a = 0; a < 3; a++) {
-			fTokens [a] = float.Parse (tokens [0]);
+		for (int b = 0; b < 3; b++) {
+			var tokens = sr.ReadLine ().Trim ().Split (',');
+			for (int a = 0; a < 3; a++) {
+				fTokens [a] = float.Parse (tokens [0]);
+			}
+
+			GameObject dynamicCube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			dynamicCube.transform.position = new Vector3 (fTokens [0], fTokens [1], fTokens [2]);
+
 		}
 
-		GameObject dynamicCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		dynamicCube.transform.position = new Vector3(fTokens[0], fTokens[1], fTokens[2]);
 
-		 
-		Debug.Log (tokens[2]);
+		//Debug.Log (tokens[2]);
 		sr.Close ();
 	}
 	
